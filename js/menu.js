6b8e83d84697f4847316449e40e90dbbd1ec0827
Nigel <nigeldcruz7@gmail.com>
2017-04-24 20:02:21 +0530

    core files
    core files of the project - initial commit

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	 $("#menu").on("click", function(){
+		 
+		$("#maim-nav ul").toggleClass("open");
+		 
+		 
+	 });
+	
+});
+
+
+
+
+//Range slider
+
+
+$(document).ready(function(){
+	
+
+	
+
+	
+});
